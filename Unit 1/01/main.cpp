c95c1ca9ee30cb6c97ad2dfe099788d80ccd0133
square_heron942 <82814680+Squareheron942@users.noreply.github.com>
2023-09-13 18:23:38 -0400

    Added pointer arithmetic bc why not

--- Unit 1/01/main.cpp
+++ Unit 1/01/main.cpp
@@ -20,7 +20,7 @@ static const char* questions[NUM_QUESTIONS] = {
 static const char* choices[NUM_QUESTIONS][MAX_CHOICES] = {
     {"int main()", "It depends on what you tell the compiler", "Whatever the first one is in the file"},
     {"char *str = NULL;", "str s = \"\";", "std::string s = \"\";"},
-    {"T::operator>>(T other) {}", "T.>>(T other) {}"},
+    {"T::operator>>(long other) {}", "T.>>(long other) {}"},
     {"int[] *function<T...>(T in...)", "template<class... T> int*(*f)(...)", "template<type T...> int[] function*(...)", "[int*](...) {}"},
     {"if", "noexcept", "co_await", "new", "not"},
     {"A number that tells the compiler how much space a variable uses", "A keyword telling the compiler how a variable should be defined, stored, and accessed"},
@@ -42,10 +42,26 @@ bool askquestion(int index) {
     char i = 0;
     printf("%u. %s\n\n", index + 1, questions[index]);
     do {printf("    %c. %s\n", i + 0x61, choices[index][i]);} while (choices[index][++i] && i < MAX_CHOICES);
-    std::cout << "\n" << std::endl;
+    printf("\n");
     do {printf("\e[1A\e[0K");getline(std::cin, answer);} while (answer.c_str()[0] < 0x61 || answer.c_str()[0] > 0x61 + MAX_CHOICES);
     answers[index] = answer.c_str()[0];
     return ((answer.c_str()[0] - 0x61) == correct[index]);
+}
+
+void doSomethingFun(void) {
+    char* items = NULL;
+    std::string numItems, item;
+    printf("Enter the number of items in a char array\n");
+    getline(std::cin, numItems);
+    items = new char[std::stoi(numItems)];
+    for (int i = 0; i < std::stoi(numItems); ++i) {
+        getline(std::cin, item);
+        items[i] = item.c_str()[0];
+    };
+    printf("Enter an offset\n\n");
+    do {printf("\e[1A\e[0K");getline(std::cin, item);} while (std::stoi(item) >= std::stoi(numItems));
+    printf("Output text: %s\n\nYou just did some pointer arithmetic!(and almost caused a segmentation fault)", items + std::stoi(item)); // add offset to pointer
+    delete[] items;
 }
 
 int main() {
@@ -56,11 +72,10 @@ int main() {
     for (int i = 0; i < NUM_QUESTIONS; ++i) {
         bool right = (answers[i] - 0x61 == correct[i]);
         printf("Your answer: %c\n%s\n",answers[i], right ? "Correct!" : "Incorrect!");
-        if (right) numright++;
-        else printf("Correct answer: %c\n", correct[i] + 0x61);
+        if (right) numright++; else printf("Correct answer: %c\n", correct[i] + 0x61);
     }
-
     printf("Score: %u/%u\n", numright, NUM_QUESTIONS);
-
+    
+    if (numright == NUM_QUESTIONS) doSomethingFun(); else printf("If you did perfect maybe you could have done something more interesting...");
     return 0;
 }
