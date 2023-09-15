a835673199988df1ad2206ee9a5e625d1c6604a8
square_heron942 <82814680+Squareheron942@users.noreply.github.com>
2023-09-15 12:21:15 -0400

    Added prng function

--- Unit 1/01/main.cpp
+++ Unit 1/01/main.cpp
@@ -1,4 +1,9 @@
 #include <iostream>
+#include <time.h>
+#include <type_traits>
+#include <bit>
+#include <concepts>
+#include <chrono>
 
 #define NUM_QUESTIONS 10
 #define MAX_CHOICES 5
@@ -44,9 +49,57 @@ static std::string answer;
 bool askquestion(int index);
 void doSomethingFun(void);
 
+unsigned long long xorshf(unsigned long long x) {
+    x ^= x >> 12; // a
+    x ^= x << 25; // b
+    x ^= x >> 27; // c
+    return x;
+}
+
+static inline double to_double(uint64_t x) {
+   const union { uint64_t i; double d; } u = {.i = UINT64_C(0x3FF) << 52 | x >> 12 };
+   return u.d - 1.0;
+}
+
+namespace randnum {
+    namespace {
+        template<typename T>
+        concept arithmetic = std::integral<T> or std::floating_point<T>;
+        static unsigned long long state = 0x6ab93aeb19a953;
+    }
+    
+
+    template<class T> requires arithmetic<T> T genrandom(long long seed = state) {
+        unsigned long t = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+        // circular shift right 16 so that the smallest section (milliseconds) which changes most will make the largest changes
+        // 0xAAAABBBBCCCCDDDD ==> 0xDDDDAAAABBBBCCCC
+        unsigned long time = (t << 48) | (t >> 16);
+        T out = 1;
+        unsigned long val = xorshf(time  seed);
+        if constexpr (std::integral<T>) out = (T)val;
+        else out = (T)to_double(val);
+        state = val;
+        return out;
+    };
+
+    // can do ~6.6 million per second on my laptop
+    template<class T> requires arithmetic<T> T genrandom(T max, T min = 0) {
+        T out = 1;
+        unsigned long val = genrandom<unsigned long>();
+        if constexpr (std::integral<T>) out = (T)((val % (max - min)) + min);
+        else out = (T)(fmodl(to_double(val), max - min) * (max - min) + min);
+        return out;
+    };
+}
+
 int main() {
     int numright(0);
     printf("\e[H\e[JWelcome to the test. There are %u questions. \n\n", NUM_QUESTIONS);
+    // printf("%lu\n", genrandom<long>(5, 1));
+    // unsigned long t1 = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+    // for (int i = 0; i < 100000000; ++i) if (!genrandom<unsigned long>()) printf("zero get\n");
+    // unsigned long t2 = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+    // printf("Took %f seconds\n", 0.001 * (t2 - t1));
     for (int i = 0; i < NUM_QUESTIONS; ++i) askquestion(i);
     printf("Mark breakdown:\n");
     for (auto &c : answers) {
@@ -55,7 +108,7 @@ int main() {
         if (right) ++numright; else printf("Correct answer: %c\n", TO_LETTER(correct[INDEX(c, answers)]));
     }
     printf("Score: %u/%u\n", numright, NUM_QUESTIONS);
-    
+    printf("You got %lu points!\n", randnum::genrandom<unsigned long>());
     if (numright == NUM_QUESTIONS) doSomethingFun(); else printf("If you did perfect maybe you could have done something more interesting...");
     return 0;
 }
@@ -69,7 +122,6 @@ bool askquestion(int index) {
     answers[index] = answer.c_str()[0];
     return ((TO_INT(answer.c_str()[0])) == correct[index]);
 }
-
 void doSomethingFun(void) {
     char* items = NULL;
     std::string numItems, item;
@@ -77,10 +129,7 @@ void doSomethingFun(void) {
     getline(std::cin, numItems);
     items = new char[std::stoi(numItems)];
     printf("Enter the items in the array\n");
-    for (int i = 0; i < std::stoi(numItems); ++i) {
-        getline(std::cin, item);
-        items[i] = item.c_str()[0];
-    };
+    for (int i = 0; i < std::stoi(numItems); ++i) { getline(std::cin, item);items[i] = item.c_str()[0]; };
     printf("Enter an offset\n\n");
     do {printf("\e[1A\e[0K");getline(std::cin, item);} while (std::stoi(item) >= std::stoi(numItems));
     printf("Output text: %s\n\nYou just did some pointer arithmetic!(and almost caused a segmentation fault)", items + std::stoi(item)); // add offset to pointer
